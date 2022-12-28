666a2d0c3a7ae53551a87370281da9d9b93f1f8b
micodee <martomhidayat>
2022-12-28 09:29:41 +0700

    12 - Membuat Styling Halaman Detail Blog

--- src/pages/blogDetail/DetailBlog.js
+++ src/pages/blogDetail/DetailBlog.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { RegisterBg } from "../../img/Img";
 
 const DetailBlog = () => {
   return (
-    <div>
-        <p>Detail Blog</p>
+    <div className="detail-blog-wrapper">
+      <img className="img-cover" src={RegisterBg} alt="thumb" loading="lazy"/>
+      <p className="blog-title">Title Blog</p>
+      <p className="blog-author">Author - Date Post</p>
+      <p className="blog-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aliquam ullam consequatur soluta nam nihil ipsum, quis asperiores. In optio eveniet deserunt id cumque ab illo recusandae minima, nobis nostrum voluptatibus pariatur, repudiandae corrupti? Alias minima obcaecati repellat quisquam neque error! Odit consequatur repudiandae, natus eligendi doloremque aspernatur molestiae quasi impedit minima perferendis quis voluptatibus, incidunt est corrupti rem, a assumenda maxime veniam iure itaque inventore aut. Laudantium quos officia, corporis repellendus maxime consectetur ipsam similique suscipit, odit inventore ab!</p>
     </div>
-  )
-}
+  );
+};
 
-export default DetailBlog
+export default DetailBlog;
